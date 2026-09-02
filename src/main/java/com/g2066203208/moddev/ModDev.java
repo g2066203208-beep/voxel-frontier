107662d91af4f24f2ee0c301d55e25dd34782947
g2066203208-beep <g2066203208@gmail.com>
2026-09-02 22:00:14 +0800

    Add minimal NeoForge mod entry point

---
+++ src/main/java/com/g2066203208/moddev/ModDev.java
@@ -0,0 +1,14 @@
+package com.g2066203208.moddev;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ModDev.MOD_ID)
+public final class ModDev {
+    public static final String MOD_ID = "moddev";
+
+    public ModDev(IEventBus modEventBus, ModContainer modContainer) {
+        // Feature registrations will be added here as the mod grows.
+    }
+}
