aab875b2dc4b31e1ca4b448ba68150e10a006eac
g2066203208-beep <g2066203208@gmail.com>
2026-09-03 17:59:48 +0800

    Add optical material refraction and diffraction primitives

--- native/include/vf/physics/SpectralOptics.hpp
+++ native/include/vf/physics/SpectralOptics.hpp
@@ -15,6 +15,17 @@ struct GameSpectrum {
     [[nodiscard]] double operator[](std::size_t index) const noexcept { return values[index]; }
 };
 
+struct OpticalMaterial {
+    GameSpectrum baseReflectance{};
+    GameSpectrum absorptionPerMeter{};
+    double roughness{0.55};
+    double transmission{};
+    double iorA{1.50};
+    double iorBMicrometerSquared{};
+    double thinFilmThicknessNm{};
+    double thinFilmIor{1.38};
+};
+
 struct FresnelPolarized {
     double s{};
     double p{};
@@ -35,12 +46,34 @@ struct StokesVector {
 [[nodiscard]] FresnelPolarized fresnelDielectric(double cosThetaIncident, double nIncident, double nTransmitted) noexcept;
 [[nodiscard]] double beerLambertTransmittance(double absorptionPerMeter, double distanceMeters) noexcept;
 [[nodiscard]] double cauchyIor(double wavelengthNm, double coefficientA, double coefficientBMicrometerSquared) noexcept;
+[[nodiscard]] bool refractDirection(
+    const glm::dvec3& incidentDirection,
+    const glm::dvec3& surfaceNormal,
+    double nIncident,
+    double nTransmitted,
+    glm::dvec3& transmittedDirection) noexcept;
+[[nodiscard]] double opticalPhaseRadians(double pathDifferenceMeters, double wavelengthNm) noexcept;
+[[nodiscard]] bool diffractionGratingAngle(
+    double wavelengthNm,
+    double grooveSpacingNm,
+    double incidentAngleRadians,
+    int order,
+    double& diffractedAngleRadians) noexcept;
 [[nodiscard]] GameSpectrum thinFilmReflectance(
     double cosThetaIncident,
     double filmThicknessNm,
     double nIncident,
     double nFilm,
     double nSubstrate) noexcept;
+[[nodiscard]] GameSpectrum materialTransmission(
+    const OpticalMaterial& material,
+    const GameSpectrum& incident,
+    double distanceMeters,
+    double cosThetaIncident = 1.0) noexcept;
+[[nodiscard]] GameSpectrum materialReflection(
+    const OpticalMaterial& material,
+    const GameSpectrum& incident,
+    double cosThetaIncident = 1.0) noexcept;
 [[nodiscard]] StokesVector linearPolarizer(const StokesVector& input, double axisRadians) noexcept;
 
 } // namespace vf
