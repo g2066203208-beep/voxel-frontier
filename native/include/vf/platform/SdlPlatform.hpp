8be30772b6c47ee0e5985d1d4b6bed3902470e17
g2066203208-beep <g2066203208@gmail.com>
2026-09-03 11:44:11 +0800

    Add native first-person input state

--- native/include/vf/platform/SdlPlatform.hpp
+++ native/include/vf/platform/SdlPlatform.hpp
@@ -8,6 +8,19 @@ struct SDL_Window;
 
 namespace vf {
 
+struct PlatformInput {
+    bool forward{};
+    bool backward{};
+    bool left{};
+    bool right{};
+    bool sprint{};
+    bool ascend{};
+    bool descend{};
+    bool mouseCaptured{true};
+    float mouseDx{};
+    float mouseDy{};
+};
+
 class SdlPlatform final {
 public:
     SdlPlatform(std::string_view title, std::int32_t width, std::int32_t height);
@@ -20,10 +33,16 @@ public:
     [[nodiscard]] SDL_Window* window() const noexcept { return window_; }
     [[nodiscard]] std::pair<std::int32_t, std::int32_t> drawableSize() const noexcept;
     [[nodiscard]] bool consumeResize() noexcept;
+    [[nodiscard]] const PlatformInput& input() const noexcept { return input_; }
+    void setWindowTitle(std::string_view title);
 
 private:
+    void setMouseCaptured(bool captured);
+    void refreshKeyboardState();
+
     SDL_Window* window_{};
     bool resized_{true};
+    PlatformInput input_{};
 };
 
 } // namespace vf
