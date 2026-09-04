e3108b8874d7a1cb5425d4ffd21e13d9b9d69ea6
g2066203208-beep <g2066203208@gmail.com>
2026-09-05 00:35:32 +0800

    Keep renderer ABI stable while sky pass is staged

--- native/include/vf/render/VulkanRenderer.hpp
+++ native/include/vf/render/VulkanRenderer.hpp
@@ -33,9 +33,7 @@ public:
         const glm::vec3& sunDirectionToLight = glm::vec3{0.38F, 0.83F, 0.41F},
         const glm::vec3& sunLinearColor = glm::vec3{1.0F},
         float sunIntensity = 2.2F,
-        const glm::dquat& staticObjectRotation = glm::dquat{1.0, 0.0, 0.0, 0.0},
-        float atmosphereDensity = 1.0F,
-        float solarElevation = 0.7F);
+        const glm::dquat& staticObjectRotation = glm::dquat{1.0, 0.0, 0.0, 0.0});
     void requestResize() noexcept { resizeRequested_ = true; }
 
     [[nodiscard]] const std::string& gpuName() const noexcept { return gpuName_; }
@@ -120,7 +118,6 @@ private:
 
     VkPipelineLayout pipelineLayout_{VK_NULL_HANDLE};
     VkPipeline graphicsPipeline_{VK_NULL_HANDLE};
-    VkPipeline skyPipeline_{VK_NULL_HANDLE};
 
     VkBuffer vertexBuffer_{VK_NULL_HANDLE};
     VkDeviceMemory vertexMemory_{VK_NULL_HANDLE};
