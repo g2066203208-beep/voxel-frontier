2d15859b3cc8027ceda16569533fada2a7625ee7
g2066203208-beep <g2066203208@gmail.com>
2026-09-05 00:59:06 +0800

    Add semantic landform profile contract

--- native/include/vf/world/detail/PlanetGenerationInternal.hpp
+++ native/include/vf/world/detail/PlanetGenerationInternal.hpp
@@ -24,6 +24,17 @@ struct SurfaceFrame {
     glm::dvec3 up{0.0, 1.0, 0.0};
 };
 
+struct LandformProfile {
+    // Broad semantic fields in [0,1]. They describe why terrain exists before fine noise is added.
+    double continent{0.5};
+    double mountainBelt{0.0};
+    double valleyCorridor{0.0};
+    double basin{0.0};
+    double plateau{0.0};
+    double forestCore{0.0};
+    double talusField{0.0};
+};
+
 struct LocalVertex {
     glm::dvec3 position{};
     glm::vec3 material{};
@@ -45,6 +56,7 @@ struct Placement {
 [[nodiscard]] double seedPhase(std::uint64_t seed, std::uint64_t channel) noexcept;
 [[nodiscard]] double valueNoise3(std::uint64_t seed, const glm::dvec3& p) noexcept;
 [[nodiscard]] double centeredFbm(std::uint64_t seed, const glm::dvec3& p, unsigned octaves) noexcept;
+[[nodiscard]] LandformProfile semanticLandform(const PlanetDefinition&, const glm::dvec3&) noexcept;
 [[nodiscard]] double terrainMoisture(const PlanetDefinition&, const glm::dvec3&) noexcept;
 [[nodiscard]] double terrainTemperature(const PlanetDefinition&, const glm::dvec3&, double normalizedHeight) noexcept;
 [[nodiscard]] glm::vec3 terrainMaterialData(const PlanetDefinition&, const glm::dvec3&, double normalizedHeight) noexcept;
